bc4d436891a4f6c82c77cb51fd0266af1be16558
michellejanosi <michelle.janosi@gmail.com>
2022-09-19 05:48:53 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+/* Requirements:
+  * Choose "seed color" with input type of color.
+  * Choose color scheme mode in a select box.
+  * Clicking button makes a request to color api (https://www.thecolorapi.com/docs#schemes) to get a color scheme.
+  * Display scheme colors and hex values on the page.
+  * Click hex values to copy to clipboard.
+*/
