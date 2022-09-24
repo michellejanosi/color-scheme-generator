101feb57909c9b42427ecec6bd578b215d5ef6c3
michellejanosi <michelle.janosi@gmail.com>
2022-09-24 08:22:11 -0400

    Add get color scheme function and render to dom

--- app.js
+++ app.js
@@ -5,3 +5,47 @@
   * Display scheme colors and hex values on the page.
   * Click hex values to copy to clipboard.
 */
+
+const form = document.querySelector('.input-group');
+let colorsArr = [];
+
+function renderColors() {
+  let colorsHtml = '';
+  let hexValuesHtml = '';
+
+  for (let color of colorsArr) {
+    colorsHtml += `
+      <div class="color"><img src="${color.image.bare}"></div>`;
+
+    hexValuesHtml += `
+      <div class="hex-value">${color.hex.value}</div>`;
+  }
+
+  const colors = document.querySelector('.colors');
+  const hexValues = document.querySelector('.hex-values');
+  colors.innerHTML = colorsHtml;
+  hexValues.innerHTML = hexValuesHtml;
+}
+
+function getScheme(hex='F15025', mode='monochrome') {
+  fetch(`https://www.thecolorapi.com/scheme?hex=${hex}&mode=${mode}&count=5`)
+    .then((response) => response.json())
+    .then((data) => {
+      colorsArr = data.colors;
+      renderColors();
+    });
+}
+
+getScheme();
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const colorPicked = document.getElementById('color').value.substring(1);
+  const colorScheme = document.getElementById('color-type').value
+  const data = {
+    color: colorPicked,
+    scheme: colorScheme
+  }
+
+  getScheme(data.color, data.scheme);
+})
